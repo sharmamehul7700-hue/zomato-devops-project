e10cc86872af8cd6cab894a0e4bea9760ff63230
Ubuntu <ubuntu@ip-172-31-46-56.ap-south-1.compute.internal>
2026-07-08 12:26:26 +0000

    Initial Zomato Spring Boot DevOps project

---
+++ src/main/java/com/zomato/ZomatoApplication.java
@@ -0,0 +1,12 @@
+package com.zomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZomatoApplication.class, args);
+    }
+}
